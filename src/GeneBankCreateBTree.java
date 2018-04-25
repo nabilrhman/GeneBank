5e306f6edf88e1830a99f4963cc6bc6a33acdbd3
nabilrhman <nabilr@outlook.com>
2018-04-24 19:51:22 -0600

    Adds inOrderPrintToWriter to BTree based on current GeneBankCreateBTree using Maruf's version. Needs modification for Austin's version.

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -12,23 +12,23 @@ import java.io.IOException;
  *
  */
 public class GeneBankCreateBTree {
-	
+
 	public static final long CODE_A = 0b00L;
 	public static final long CODE_T = 0b11L;
 	public static final long CODE_C = 0b01L;
 	public static final long CODE_G = 0b10L;
-	
+
 	public static final int MAX_SEQUENCE_LENGTH = 31;
 	public static final int MAX_DEBUG_LEVEL = 1;
-	
+
 	public static void main(String[] args) throws IOException {
 		if (args.length < 4) {
 			badUsage();
 		}
-		
+
 		// <cache>
 		boolean useCache = false;
-		
+
 		try {
 			int c = Integer.parseInt(args[0]);
 			if (c == 0) useCache = false;
@@ -37,10 +37,10 @@ public class GeneBankCreateBTree {
 		} catch (NumberFormatException e) {
 			badUsage();
 		}
-		
+
 		// <degree>
 		int BTreeDegree = 0;
-		
+
 		try {
 			int deg = Integer.parseInt(args[1]);
 			if (deg < 0) badUsage();
@@ -50,7 +50,7 @@ public class GeneBankCreateBTree {
 			badUsage();
 		}
 		int sequenceLength = 0;
-		
+
 		// <sequence length>
 		try {
 			int len = Integer.parseInt(args[3]);
@@ -59,11 +59,11 @@ public class GeneBankCreateBTree {
 		} catch (NumberFormatException e) {
 			badUsage();
 		}
-		
+
 		// [<cache size>] [<debug level>]
 		int cacheSize = 0;
 		int debugLevel = 0;
-		
+
 		if (args.length > 4) {
 			if (useCache) {
 				try {
@@ -84,7 +84,7 @@ public class GeneBankCreateBTree {
 				}
 			}
 		}
-		
+
 		// <gbk file>
 		File gbk = new File(args[2]);
 
@@ -97,14 +97,14 @@ public class GeneBankCreateBTree {
 		}
 	    String BTreeFile = (gbk + ".btree.data." + sequenceLength + "." + BTreeDegree);
 		BTree tree = new BTree(BTreeDegree, BTreeFile, useCache, cacheSize);
-		
+
         String line = null;
 		line = in.readLine().toLowerCase().trim();
 		boolean inSequence = false;
 		int sequencePosition = 0;
 		int charPosition = 0;
 		long sequence = 0L;
-        
+
         while (line != null) { // tried to optimize this, mostly made it ugly
             if (inSequence) {
                 if (line.startsWith("//")) { // marks the end of the sequence
@@ -159,14 +159,14 @@ public class GeneBankCreateBTree {
 			tree.inOrderPrintToWriter(tree.getRoot(),writer,sequenceLength);
 			writer.close();
 		}
-		
+
 		// we're done modifying the tree, finalize it
 		if (useCache) tree.flushCache();
-		
-	    //tree.inOrderPrint(tree.getRoot());	
+
+	    //tree.inOrderPrint(tree.getRoot());
 		in.close();
 	}
-	
+
 	private static void badUsage() {
 		System.err.println("Usage: java GeneBankCreateBTree <cache> <degree> <gbk file> <sequence length> [<cache size>] [<debuglevel>]");
 		System.err.println("<cache>: 1 to use cache, 0 for no cache");
