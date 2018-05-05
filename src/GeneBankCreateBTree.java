fddaf0fe1d11ef4d119b4417b9cc03ebfa192b85
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2018-05-04 22:28:19 -0600

    Update GeneBankCreateBTree.java

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -1,85 +1,198 @@
-  * Creates a BTree from a user provided gbk file.
-   * @author nabilr
-   */
- +import java.io.File;
- +import java.io.IOException;
- +import java.io.RandomAccessFile;
- +import java.util.Scanner;
-  public class GeneBankCreateBTree
-  {
- +static int degree;
- +	static int subseq;
- +	static String fname;
- +	static File readFile;
- +	static Scanner scan;
- +	static BTree myBTree;
- +	static Parser myparse;
- +	static Boolean Flag=false;
- +	public static void main(String[] args) throws IOException {
- +		readFile=new File("treefile");
- +		scan=new Scanner(readFile);
- +		if (Integer.parseInt(args[0]) == 0) {
- +			degree = 510;
- +		} else {
- +			degree = Integer.parseInt(args[0]);
- +		}
- +		if (args[1].contains(".gbk")) {
- +				fname=args[1];
- +		} else {
- +			System.err.println("ERROR: Invalid Gene Bank File (must include .gbk)");
- +			System.exit(-1);
- +		}
- +		if (Integer.parseInt(args[2]) >= 1 && (Integer.parseInt(args[2]) <= 31)) {
- +			subseq = Integer.parseInt(args[2]);
- +		} else {
- +			System.err.println("ERROR: Sub-Sequence OverFlow to apply this program correctly please input" + "\n"
- +					+ "an integer vale between the values of 1 and 31 inclusively");
- +			System.exit(-1);
- +		}
- +
- +		myBTree = new BTree(degree, fname);
- +		myparse=new Parser(myBTree);
- +		StringBuilder s = null;
- +		while(scan.hasNext()&&!scan.hasNext("//"))
- +		{
- +			
- +			if(!scan.hasNext("ORIGIN")&&Flag==false)
- +			{
- +				scan.nextLine();
- +			}else{
- +				Flag=true;
- +				while(scan.hasNext("ORIGIN")||scan.hasNextInt())
- +				{
- +					scan.next();
- +				}
- +				if(s==null||scan.hasNext(" "))
- +				{
- +					s=new StringBuilder();
- +					while(s.length()<subseq)
- +					{
- +					s=s.append(scan.next());
- +					}
- +				}
- +				else if(s.length()<subseq)
- +				{
- +				while(s.length()<subseq){
- +					if(scan.hasNextInt())
- +					{
- +						scan.next();
- +					}
- +					s=s.append(scan.next());
- +				}
- +				System.out.println(s);
- +				}
- +				int i=0;
- +				while(i<(s.length()-subseq))
- +						{
- +				myBTree.BTreeInsert(myparse.subseqtolong(s.substring(i, subseq+i),subseq));
- +					i++;
- +						}
- +				s=s.delete(0,s.length());
- +			}
- +		}
- +	
- +	}
-  }
+  import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+/**
+ * Create a BTree from a gbk file.
+ * 
+ * @author marufahmed
+ *
+ */
+public class GeneBankCreateBTree {
+
+	public static final long CODE_A = 0b00L;
+	public static final long CODE_T = 0b11L;
+	public static final long CODE_C = 0b01L;
+	public static final long CODE_G = 0b10L;
+
+	public static final int MAX_SEQUENCE_LENGTH = 31;
+	public static final int MAX_DEBUG_LEVEL = 1;
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 4) {
+			badUsage();
+		}
+
+		// <cache>
+		boolean useCache = false;
+
+		try {
+			int c = Integer.parseInt(args[0]);
+			if (c == 0) useCache = false;
+			else if (c == 1) useCache = true;
+			else badUsage();
+		} catch (NumberFormatException e) {
+			badUsage();
+		}
+
+		// <degree>
+		int BTreeDegree = 0;
+
+		try {
+			int deg = Integer.parseInt(args[1]);
+			if (deg < 0) badUsage();
+			else if (deg == 0) BTreeDegree = getOptimalDegree();
+			else BTreeDegree = deg;
+		} catch (NumberFormatException e) {
+			badUsage();
+		}
+		int sequenceLength = 0;
+
+		// <sequence length>
+		try {
+			int len = Integer.parseInt(args[3]);
+			if (len < 1 || len > MAX_SEQUENCE_LENGTH) badUsage();
+			else sequenceLength = len;
+		} catch (NumberFormatException e) {
+			badUsage();
+		}
+
+		// [<cache size>] [<debug level>]
+		int cacheSize = 0;
+		int debugLevel = 0;
+
+		if (args.length > 4) {
+			if (useCache) {
+				try {
+					int csize = Integer.parseInt(args[4]);
+					if (csize < 1) badUsage();
+					else cacheSize = csize;
+				} catch (NumberFormatException e) {
+					badUsage();
+				}
+			}
+			if (!useCache || args.length > 5) {
+				try {
+					int dlevel = Integer.parseInt(useCache ? args[5] : args[4]);
+					if (dlevel < 0 || dlevel > MAX_DEBUG_LEVEL) badUsage();
+					else debugLevel = dlevel;
+				} catch (NumberFormatException e) {
+					badUsage();
+				}
+			}
+		}
+
+		// <gbk file>
+		File gbk = new File(args[2]);
+
+		// XXX: there's got to be a better class for this.
+		BufferedReader in = null;
+		Parser parse=new Parser();
+		try {
+			in = new BufferedReader(new FileReader(gbk));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: "+gbk.getPath());
+		}
+	    String BTreeFile = (gbk + ".btree.data." + sequenceLength + "." + BTreeDegree);
+		BTree tree = new BTree(BTreeDegree, BTreeFile, useCache, cacheSize);
+
+        String line = null;
+		line = in.readLine().toLowerCase().trim();
+		boolean inSequence = false;
+		int sequencePosition = 0;
+		int charPosition = 0;
+		long sequence = 0L;
+
+        while (line != null) { // tried to optimize this, mostly made it ugly
+            if (inSequence) {
+                if (line.startsWith("//")) { // marks the end of the sequence
+                    inSequence = false;
+                    sequence = 0;
+                    sequencePosition = 0;
+                } else {
+                	
+                	while (charPosition< line.length()) {
+                		
+                		char c = line.charAt(charPosition++);
+                        switch (c) {
+	                        case 'a':
+	                            sequence = ((sequence<<2) | CODE_A);
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
+	                            break;
+	                        case 't':
+	                            sequence = ((sequence<<2) | CODE_T);
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
+	                            break;
+	                        case 'c':
+	                            sequence = ((sequence<<2) | CODE_C);
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
+	                            break;
+	                        case 'g':
+	                            sequence = ((sequence<<2) | CODE_G);
+	                            if (sequencePosition < sequenceLength) sequencePosition++;
+	                            break;
+	                        case 'n': // end of subsequence
+	                            sequencePosition = 0;
+                                sequence = 0;
+	                            continue;
+	                        default: // space or number, not part of sequence
+	                            continue;
+                        }
+                        if (sequencePosition >= sequenceLength) {
+	                        tree.insert(sequence & (~(0xffffffffffffffffL<<(sequenceLength<<1))));
+                        }
+                     
+         
+                	}
+                	
+                }
+            } else if (line.startsWith("ORIGIN")) { // marks the beginning of a sequence
+	            inSequence = true;
+			}
+            // not bothering with the rest of the fields for now
+            line = in.readLine();
+            charPosition = 0;
+		}
+		// print debug dump
+		if (debugLevel > 0) {
+			File dumpFile = new File("dump");
+			dumpFile.delete();
+			dumpFile.createNewFile();
+			PrintWriter writer = new PrintWriter(dumpFile);
+			tree.inOrderPrint(tree.getRoot());
+			writer.close();
+		}
+
+		// we're done modifying the tree, finalize it
+		if (useCache) tree.flushCache();
+
+	    //tree.inOrderPrint(tree.getRoot());
+		in.close();
+	}
+
+	private static void badUsage() {
+		System.err.println("Usage: java GeneBankCreateBTree <cache> <degree> <gbk file> <sequence length> [<cache size>] [<debuglevel>]");
+		System.err.println("<cache>: 1 to use cache, 0 for no cache");
+		System.err.println("<degree>: degree of the BTree (0 for default)");
+		System.err.println("<gbk file>: file with sequence data");
+		System.err.println("<sequence length>: length of a sequence (1-31)");
+		System.err.println("[<cache size>]: if cache enabled, size of cache");
+		System.err.println("[<debug level>]: debugging level (0-1)");
+		System.exit(1);
+    }
+	public static int getOptimalDegree(){
+        double optimum;
+        int sizeOfPointer = 4;
+        int sizeOfObject = 12;
+        int sizeOfMetadata = 5;
+        double diskBlockSize = optimum = 4096;
+        optimum += sizeOfObject;
+        optimum -= sizeOfPointer;
+        optimum -= sizeOfMetadata;
+        optimum /= (2 * (sizeOfObject + sizeOfPointer));
+        return (int) Math.floor(optimum);
+    }
+}
